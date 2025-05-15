ce42edbc3f38c7fb4f3e09333aaabbb54e5a06c3
Pranav Embran S <pranavembrans@gmail.com>
2025-05-15 18:14:18 +0530

    Remove ignored files from Git tracking

--- Other/crud/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_POST['delete'])) {
-    $delete_id = $_POST['delete_id'];
-
-    $sql_delete = "DELETE FROM studentdetails WHERE id='$delete_id'";
-    $conn->query($sql_delete);
-}
-?>
